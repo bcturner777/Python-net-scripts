afbc6163c1710a98f203bbab828f44e212f5a03f
papaT <blaine_turner@mentor.com>
2016-11-18 10:34:46 -0800

    netmiko command runner

---
+++ command_runner.py
@@ -0,0 +1,18 @@
+import netmiko
+import json
+
+raw_input("Enter file name: ")
+
+devices = '''
+tahwangw1
+tahwanex1
+'''.strip().splitlines()
+
+device_type = 'cisco_ios'
+username = raw_input("Enter device list username: ")
+password = raw_input("Enter device list password: ")
+
+for device in devices :
+    connection = netmiko.ConnectHandler(ip=device, device_type=device_type, username=username, password=password)
+    print(connection.send_command('show clock'))
+    connection.disconnect()
