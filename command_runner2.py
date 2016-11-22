e849840d81273320d7e6a6c77e3c2b50ba4a6d95
papaT <blaine_turner@mentor.com>
2016-11-22 08:51:58 -0800

    Added prompt to input a specific command

--- command_runner2.py
+++ command_runner2.py
@@ -7,6 +7,7 @@ device_group = raw_input("Enter device list seperated by space: ")
 
 device_type = 'cisco_ios'
 username = raw_input("Enter device list username: ")
+command = raw_input("Enter specific show command: ")
 password = getpass.getpass(prompt='Enter device list password: ')
 
 netmiko_exceptions = (netmiko.ssh_exception.NetMikoTimeoutException, netmiko.ssh_exception.NetMikoAuthenticationException)
@@ -18,7 +19,7 @@ for name in device_group.split():
         print('-'*79)
         print('Connecting to device', device, name)
         connection = netmiko.ConnectHandler(ip=device, device_type=device_type, username=username, password=password)
-        print(connection.send_command('show clock'))
+        print(connection.send_command(command))
         connection.disconnect()
     except netmiko_exceptions as e:
         print('Failed to ', device, e)
