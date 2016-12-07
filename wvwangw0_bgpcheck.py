69acee5fa7da3b3b3aeb22a5cc0b0895c934c950
papaT <blaine_turner@mentor.com>
2016-12-07 09:25:25 -0800

    add wvwangw0 program

---
+++ wvwangw0_bgpcheck.py
@@ -0,0 +1,25 @@
+import netmiko
+import json
+import socket
+import getpass
+
+device_group = raw_input("Enter device list seperated by space: ")
+
+device_type = 'cisco_ios'
+username = raw_input("Enter device list username: ")
+password = getpass.getpass(prompt='Enter device list password: ')
+command = raw_input("Enter specific show command: ")
+
+netmiko_exceptions = (netmiko.ssh_exception.NetMikoTimeoutException, netmiko.ssh_exception.NetMikoAuthenticationException)
+
+for name in device_group.split():
+    name = name.strip()
+    device = socket.gethostbyname(name)
+    try :
+        print('-'*79)
+        print('Connecting to device', device, name)
+        connection = netmiko.ConnectHandler(ip=device, device_type=device_type, username=username, password=password)
+        print(connection.send_command(command))
+        connection.disconnect()
+    except netmiko_exceptions as e:
+        print('Failed to ', device, e)
